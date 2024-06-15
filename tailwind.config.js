17cb1b6a5780bd22bd7b8afea4570977b3c20cef
Muhammad Sameer Naveed <68469246+Sameer-dot@users.noreply.github.com>
2024-06-15 18:53:48 +0500

    Initial Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "purple": "#635FC7",
+        "purple-hover": "#A8A4FF",
+        red: "#EA5555",
+        "red-hover": "#FF9898",
+        "medium-gray": "#828FA3",
+        "dark-gray": "#2B2C37",
+        "very-dark-gray": "#20212C",
+        "light-lines": "#E4EBFA",
+        "dark-lines": "#3E3F4E",
+        "background-light": "#F4F7FD",
+        "background-dark": "#000000",
+        white: "#FFFFFF",
+        black: "#000112",
       },
     },
   },
